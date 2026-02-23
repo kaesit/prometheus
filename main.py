c999ce5bcf404d315c4ce6baabc8f7b635feb009
= <eakosedag@gmail.com>
2026-02-23 20:42:42 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from rich.console import Console
+from rich.progress import track
+from rich.table import Table
+import time
+def main():
+    console  = Console()
+    
+    console.print("Hello from Prometheus!")
+    console.print("Hello from Prometheus", style="bold")
+    console.print("Hello from Prometheus", style="bold underline")
+    console.print("Hello from Prometheus", style="bold underline green")
+    
+    console.print("Hello from Prometheus", style="bold underline blue on red")
+    
+    console.print("[bold]Hello [cyan]from[/] Prometheus[/]")
+    for i in track(range(10), description="Processing..."):
+        print(f"working {i}")
+        time.sleep(0.5)
+    table = Table(title="Star Wars Movies")
+    table.add_column("Released", style="cyan")
+    table.add_column("Title", style="magenta")
+    table.add_column("Released", style="green")
+    
+    table.add_row("Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$952,100,678")
+    table.add_row("May 25, 2018", "Solo: A Star Wars Story", "$352,100,678")
+    table.add_row("Dec 15, 2017", "Star Wars Ep. V111: The Last Jedi", "$1,332,539,889")
+    table.add_row("Dec 16, 2016", "Rogue One: A Star Wars Story", "$1,332,519,889")
+    console.print(table)
+    
+    
+    
+
+
+if __name__ == "__main__":
+    main()
